f7bbf7a80dc94346c71ec8b6ed61059e9271c283
Jessie Waite and Rakhee Gandhi <jessie@>
2017-09-20 11:50:38 -0700

    Add js for adding -way to the end of words starting with a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,23 @@ $(document).ready(function() {
     var input = $("input#original-input").val();
     var vowels= ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
     var inputArray= [""]
-      for (var i = 0; i < input.length; i++) {
-      if (vowels.includes(input.charAt(i))) {
-        inputArray.push(input.charAt(i) + "way");
-      }
+
+// Code for single letter word beginning vowel
+    //   for (var i = 0; i < input.length; i++) {
+    //   if (vowels.includes(input.charAt(i))) {
+    //     inputArray.push(input.charAt(i) + "way");
+    //   }
+    // }
+// Code for multi-letter word beginning with vowel
+    //for (var i = 0; i < 2;) {
+
+// Code for multi and single letter words beginnign with vowel
+    if (vowels.includes(input.charAt(0))) {
+      inputArray.push(input + "way");
+    } else {
+      inputArray.charAt(0).splice(0 , 1)
     }
+
 
     $("#output").text(inputArray.join(" "));
   });
